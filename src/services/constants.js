5d42f98b6e07451816ad89f0ed1865dae7ca6b41
Richard <richardstuart007@hotmail.com>
2022-09-06 21:33:45 +1200

    init

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,7 +1,7 @@
 exports.SQL_TABLE = 'questions'
 exports.URL_BASE = 'http://localhost:10008'
 exports.URL_TABLES = '/QuizTables'
-exports.SQL_ROWS = 500
+exports.SQL_ROWS = 2000
 exports.SQL_TABLE_QUESTIONS = 'questions'
 exports.SQL_TABLE_BIDDING = 'bidding'
 exports.SQL_TABLE_HANDS = 'hands'
